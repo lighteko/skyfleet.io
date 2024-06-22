e638d9a5687f7011d68b245a6c8f3838069c5c23
lighteko <huijunyi0212@naver.com>
2024-06-22 19:04:17 +0900

    Player Movement using Netcode

--- Jet.cs
+++ Jet.cs
@@ -5,14 +5,6 @@ public class Jet : MonoBehaviour
 {
     void FixedUpdate()
     {
-        RotateToMouseCursor();
-    }
 
-    void RotateToMouseCursor()
-    {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePosition - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
     }
 }
---
+++ PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class Player : NetworkBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 0;
+    private float angle = 0;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!IsOwner) return;
+
+        Vector2 worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = ((Vector3)worldMousePosition - transform.position).normalized;
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Vector3 currentPosition = transform.position;
+        SendMovementServerRpc(currentPosition, direction, speed * 0.1f, angle); 
+    }
+
+    [ServerRpc]
+    public void SendMovementServerRpc(Vector3 pos, Vector3 dir, float speed, float angle)
+    {
+        transform.position = new Vector3(pos.x + dir.x * speed, pos.y + dir.y * speed, 0);
+        transform.Find("Jet").transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+    }
+}
