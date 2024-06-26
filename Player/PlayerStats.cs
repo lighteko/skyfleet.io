a3bc82f1ff33bb3075ac3adb4e8d09cad0ec20df
lighteko <huijunyi0212@naver.com>
2024-06-26 23:23:53 +0900

    stats init

--- Player/PlayerStats.cs
+++ Player/PlayerStats.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class PlayerStats : NetworkBehaviour
 {
-    private NetworkVariable<PlayerStatsState> _playerStatsState;
     private NetworkVariable<FixedString32Bytes> _id;
     public NetworkVariable<FixedString32Bytes> Id { get => _id; set => _id = value; }
+
+    #region stats
+    private NetworkVariable<short> _health, _fuel, _ammo, _maxHealth, _maxFuel, _level;
+    private NetworkVariable<float> _healthRegen, _fuelEfficiency, _exp, _movementSpeed;
+
+    public NetworkVariable<short> Health { get => _health; set => _health = value; }
+    public NetworkVariable<short> Fuel { get => _fuel; set => _fuel = value; }
+    public NetworkVariable<short> Ammo { get => _ammo; set => _ammo = value; }
+    public NetworkVariable<short> MaxHealth { get => _maxHealth; set => _maxHealth = value; }
+    public NetworkVariable<short> MaxFuel { get => _maxFuel; set => _maxFuel = value; }
+    public NetworkVariable<float> HealthRegen { get => _healthRegen; set => _healthRegen = value; }
+    public NetworkVariable<float> FuelEfficiency { get => _fuelEfficiency; set => _fuelEfficiency = value; }
+    public NetworkVariable<float> Exp { get => _exp; set => _exp = value; }
+    public NetworkVariable<float> MovementSpeed { get => _movementSpeed; set => _movementSpeed = value; }
+    public NetworkVariable<short> Level { get => _level; set => _level = value; }
+
+    #endregion
+
     [SerializeField] private bool _usingServerAuth;
 
     void Awake()
     {
         var writePerm = _usingServerAuth ? NetworkVariableWritePermission.Server : NetworkVariableWritePermission.Owner;
-        _playerStatsState = new(readPerm: NetworkVariableReadPermission.Everyone, writePerm: writePerm);
+        var readPerm = NetworkVariableReadPermission.Everyone;
+
+        _health = new(readPerm: readPerm, writePerm: writePerm);
+        _fuel = new(readPerm: readPerm, writePerm: writePerm);
+        _ammo = new(readPerm: readPerm, writePerm: writePerm);
+        _maxHealth = new(readPerm: readPerm, writePerm: writePerm);
+        _maxFuel = new(readPerm: readPerm, writePerm: writePerm);
+        _level = new(readPerm: readPerm, writePerm: writePerm);
+
+        _healthRegen = new(readPerm: readPerm, writePerm: writePerm);
+        _fuelEfficiency = new(readPerm: readPerm, writePerm: writePerm);
+        _exp = new(readPerm: readPerm, writePerm: writePerm);
+        _movementSpeed = new(readPerm: readPerm, writePerm: writePerm);
+
         Id = new();
     }
 
@@ -21,7 +51,7 @@ public class PlayerStats : NetworkBehaviour
         SendIdServerRpc($"Player {OwnerClientId}");
         if (IsOwner)
         {
-            InitializeStats();
+            InitializeStatsServerRpc();
         }
     }
 
@@ -31,49 +61,20 @@ public class PlayerStats : NetworkBehaviour
         Id.Value = id;
     }
 
-    private void InitializeStats()
+    [ServerRpc]
+    private void InitializeStatsServerRpc()
     {
-        var stats = new PlayerStatsState {
-            Health = 100,
-            Fuel = 100,
-            MaxHealth = 100,
-            MaxFuel = 100,
-            Ammo = 100,
-            MovementSpeed = 0.8f,
-            HealthRegen = 1,
-            FuelEfficiency = 0.2f,
-            Exp = 0,
-        };
-        _playerStatsState.Value = stats;
+        Health.Value = 100;
+        Fuel.Value = 100;
+        MaxHealth.Value = 100;
+        MaxFuel.Value = 100;
+        Ammo.Value = 100;
+        MovementSpeed.Value = 0.8f;
+        HealthRegen.Value = 1;
+        FuelEfficiency.Value = 0.2f;
+        Exp.Value = 0;
+        Level.Value = 0;
     }
 
-    private struct PlayerStatsState : INetworkSerializable
-    {
-        private int _health, _fuel, _ammo;
-        private int _maxHealth, _maxFuel;
-        private float _healthRegen, _fuelEfficiency, _exp, _movementSpeed;
-
-        public int Health { readonly get => _health; set => _health = value; }
-        public int Fuel { readonly get => _fuel; set => _fuel = value; }
-        public int Ammo { readonly get => _ammo; set => _ammo = value; }
-        public int MaxHealth { readonly get => _maxHealth; set => _maxHealth = value; }
-        public int MaxFuel { readonly get => _maxFuel; set => _maxFuel = value; }
-        public float MovementSpeed { readonly get => _movementSpeed; set => _movementSpeed = value; }
-        public float HealthRegen { readonly get => _healthRegen; set => _healthRegen = value; }
-        public float FuelEfficiency { readonly get => _fuelEfficiency; set => _fuelEfficiency = value; }
-        public float Exp { readonly get => _exp; set => _exp = value; }
-
-        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
-        {
-            serializer.SerializeValue(ref _health);
-            serializer.SerializeValue(ref _fuel);
-            serializer.SerializeValue(ref _ammo);
-            serializer.SerializeValue(ref _maxHealth);
-            serializer.SerializeValue(ref _maxFuel);
-            serializer.SerializeValue(ref _healthRegen);
-            serializer.SerializeValue(ref _fuelEfficiency);
-            serializer.SerializeValue(ref _movementSpeed);
-            serializer.SerializeValue(ref _exp);
-        }
-    }
+    
 }
